3f26104ceac8e6bd7bf35c4d3b8edf96f8d74efa
AdrianoCarusoEng <adriano.caruso@eng.it>
2023-03-15 18:20:19 +0100

    prova c

---
+++ prova.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+from IPython.display import display
+
+df = pd.read_csv("C:\\Users\\E002926\\Desktop\\PE_PUNTI_MISURA_202303101536.csv" , sep=',', header=0)
+
+display(df.head)
